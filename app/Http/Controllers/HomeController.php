7a51f7256001bfa32e53fe09dde2c1c03f8de0de
yoyoypy <55522331+yoyoypy@users.noreply.github.com>
2022-03-27 21:01:32 +0700

    fix order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     public function index()
     {
         // $blogs      = Blog::all();
-        $products   = Product::orderBy('id')->with('Galleries')->orderBy('position')->get();
+        $products   = Product::orderBy('position')->with('Galleries')->get();
         $categories = Category::with('Product')->get();
 
         return view('frontend.home')->with([
